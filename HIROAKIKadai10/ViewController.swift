50443029e457cfb80358422323d9b4ed60cd8449
hirokaissh <hirokai.saito1014@gmail.com>
2022-01-25 17:01:38 +0900

    Cell の追加

--- HIROAKIKadai10/ViewController.swift
+++ HIROAKIKadai10/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as UITableViewCell
+        return cell
+    }
 }
-
