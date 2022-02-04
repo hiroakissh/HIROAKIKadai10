29bd13d2322ada1fa45c1c3165b4eafe9255837c
hirokaissh <hirokai.saito1014@gmail.com>
2022-02-04 21:40:02 +0900

    条件でCellに反映をさせた

--- HIROAKIKadai10/ViewController.swift
+++ HIROAKIKadai10/ViewController.swift
@@ -7,18 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak private var japanAreaCellLabel1: UILabel!
+    @IBOutlet weak private var japanAreaCellLabel2: UILabel!
+    @IBOutlet weak private var japanAreaCellLabel3: UILabel!
+    @IBOutlet weak private var cellNumberLabel1: UILabel!
+    @IBOutlet weak private var cellNumberLabel2: UILabel!
+    @IBOutlet weak private var cellNumberLabel3: UILabel!
+
+    let japanArea: [String] = [
+        "北海道", "青森県", "3" ,"1" ,"2" ,"3", "3" ,"1" ,"2" ,"3", "3" ,"1" ,"2" ,"3", "3" ,"1" ,"2" ,"3", "3" ,"1" ,"2"
+        ,"3", "3" ,"1" ,"2" ,"3", "3" ,"1" ,"2" ,"3", "3" ,"1" ,"2" ,"3" ,"1" ,"2" ,"3" ,"1" ,"2" ,"3" ,"1" ,"2" ,"3" ,"1" ,"2" ,"3", "1"
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+print(japanArea.count)
+        return japanArea.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as UITableViewCell
+        var identifier = ""
+
+        if indexPath.row % 3 == 0 {
+            identifier = "Cell1"
+        } else if indexPath.row % 3 == 1 {
+            identifier = "Cell2"
+        } else if indexPath.row % 3 == 2 {
+            identifier = "Cell3"
+        }
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         return cell
     }
 }
