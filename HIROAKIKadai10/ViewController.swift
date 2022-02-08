3732a5f5a4e7c0d6abea17ea621cba22250f22c9
hirokaissh <hirokai.saito1014@gmail.com>
2022-02-08 19:50:18 +0900

    SwiftLintの修正

--- HIROAKIKadai10/ViewController.swift
+++ HIROAKIKadai10/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
     let japanArea: [String] = [
         "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県",
         "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県",
