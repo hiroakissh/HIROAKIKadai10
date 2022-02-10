bd78527a44d458bec948b81b258ebfbaed219daf
hirokaissh <hirokai.saito1014@gmail.com>
2022-02-10 22:31:58 +0900

    あきおさんのフィードバック反映

--- HIROAKIKadai10/ViewController.swift
+++ HIROAKIKadai10/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let japanArea: [String] = [
+    private let japanAreas: [String] = [
         "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県",
         "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県",
         "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
@@ -19,26 +19,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return japanArea.count
+        japanAreas.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var identifier = ""
-
-        if indexPath.row % 3 == 0 {
-            identifier = "Cell1"
-        } else if indexPath.row % 3 == 1 {
-            identifier = "Cell2"
-        } else if indexPath.row % 3 == 2 {
-            identifier = "Cell3"
-        }
-
+        let identifiers: [String] = ["Cell1", "Cell2", "Cell3"]
+        let identifier = identifiers[indexPath.row % identifiers.count]
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-        cell.textLabel?.text = japanArea[indexPath.row]
+        cell.textLabel?.text = japanAreas[indexPath.row]
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です。"
         return cell
     }
